c047eb09a62cfcf3c8c82ef267834e5c2f96a588
Robert Guss <robert.guss@insuremytrip.com>
2019-03-14 10:29:41 -0400

    pre commit test

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,5 @@ import 'airbnb-browser-shims';
 import '../scss/main.scss';
 
 if (process.env.NODE_ENV !== 'production') {
-  console.log('Looks like we are in development mode!');
+  console.log ("Looks like we are in development mode!");
 }
