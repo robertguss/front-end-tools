967517d493a8bbe4e87f11b6fc86a8a363a31785
Robert Guss <robert.guss@insuremytrip.com>
2019-03-14 11:10:45 -0400

    configured webpack dev server and browsersync

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,14 +1,15 @@
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 const merge = require('webpack-merge');
 const WebpackNotifierPlugin = require('webpack-notifier');
-// const WriteFilePlugin = require('write-file-webpack-plugin');
+const path = require('path');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist',
+    contentBase: path.join(__dirname, 'dist'),
+    port: 9000,
   },
   module: {
     rules: [
@@ -37,19 +38,19 @@ module.exports = merge(common, {
   plugins: [
     // https://www.npmjs.com/package/webpack-notifier
     new WebpackNotifierPlugin(),
-    // https://github.com/gajus/write-file-webpack-plugin
-    // new WriteFilePlugin({
-    //   useHashIndex: true,
-    // }),
     // https://github.com/Va1/browser-sync-webpack-plugin
     new BrowserSyncPlugin(
       // BrowserSync options
       {
         host: 'localhost',
+        open: false,
         port: 3000,
-        proxy: 'http://localhost:8080/',
+        proxy: 'http://localhost:9000/',
       },
+      // plugin options
       {
+        // prevent BrowserSync from reloading the page
+        // and let Webpack Dev Server take care of this
         reload: false,
       }
     ),
