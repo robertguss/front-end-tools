aa3b337f719231da56e7fd357dbbb1d0683256e8
Robert Guss <robert.guss@insuremytrip.com>
2019-03-20 08:59:13 -0400

    removed testing scripts from lint-staged. modified husky to run lint-staged on pre-push instead of pre-commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -7,7 +7,6 @@ module.exports = {
       'npm run lint:stylelint',
       'npm run lint:prettier',
       'git add',
-      // 'npm run test:unit:file',
     ],
     '*.scss': ['npm run lint:stylelint', 'npm run lint:prettier', 'git add'],
     '*.md': ['npm run lint:markdownlint', 'npm run lint:prettier', 'git add'],
