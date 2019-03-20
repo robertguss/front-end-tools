a04e262f4e9973c6afbe861c80b4bac78649983e
Robert Guss <robert.guss@insuremytrip.com>
2019-03-20 07:17:23 -0400

    modified jest config by adding root dir locations for jest

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.{js,vue}', '!**/node_modules/**'],
   coverageDirectory: '<rootDir>/tests/unit/coverage',
   moduleFileExtensions: ['js', 'json', 'vue'],
+  roots: ['<rootDir>/src/', '<rootDir>/tests/'],
   testMatch: ['**/(*.)spec.js'],
   transform: {
     '^.+\\.vue$': 'vue-jest',
