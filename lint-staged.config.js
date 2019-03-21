55c0be1e71b26c9aa20851a244ec23f43baf3c27
Robert Guss <robert.guss@insuremytrip.com>
2019-03-21 10:50:50 -0400

    added prettierrc.js

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   linters: {
-    "**/*.js": ["npm run lint:eslint", "npm run lint:prettier", "git add"],
-    "package.json": ["npm run lint:prettier", "git add"],
-    "*.vue": [
-      "npm run lint:eslint",
-      "npm run lint:stylelint",
-      "npm run lint:prettier",
-      "git add"
+    '**/*.js': ['npm run lint:eslint', 'npm run lint:prettier', 'git add'],
+    'package.json': ['npm run lint:prettier', 'git add'],
+    '*.vue': [
+      'npm run lint:eslint',
+      'npm run lint:stylelint',
+      'npm run lint:prettier',
+      'git add',
     ],
-    "*.scss": ["npm run lint:stylelint", "npm run lint:prettier", "git add"],
-    "*.md": ["npm run lint:markdownlint", "npm run lint:prettier", "git add"]
-  }
+    '*.scss': ['npm run lint:stylelint', 'npm run lint:prettier', 'git add'],
+    '*.md': ['npm run lint:markdownlint', 'npm run lint:prettier', 'git add'],
+  },
 };
