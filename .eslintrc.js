75d13c1fa708ed3956bde4519e6deeea38f6f1e5
Robert Guss <robert.guss@insuremytrip.com>
2019-03-21 10:48:44 -0400

    updated eslintrc to use prettier/standard last

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,43 +2,39 @@
 // https://github.com/wesbos/eslint-config-wesbos
 // https://vuejs.github.io/eslint-plugin-vue/user-guide/#faq
 module.exports = {
-  'extends': [
-    'airbnb-base',
-    'plugin:vue/recommended',
-    'prettier/standard',
-    'prettier/vue',
+  extends: [
+    "airbnb-base",
+    "plugin:vue/recommended",
+    "prettier/vue",
+    "prettier/standard"
   ],
-  'parserOptions': {
-    'ecmaVersion': 2019,
-    'ecmaFeatures': {
-      'impliedStrict': true
+  parserOptions: {
+    ecmaVersion: 2019,
+    ecmaFeatures: {
+      impliedStrict: true
     },
-    'parser': 'babel-eslint',
+    parser: "babel-eslint"
   },
-  'env': {
-    'browser': true,
-    'node': true,
-    'jest': true
+  env: {
+    browser: true,
+    node: true,
+    jest: true
   },
-  'rules': {
-    'vue/component-name-in-template-casing': [
-      'error',
-      'PascalCase',
+  rules: {
+    "vue/component-name-in-template-casing": [
+      "error",
+      "PascalCase",
       {
         ignores: [
-          'component',
-          'template',
-          'transition',
-          'transition-group',
-          'keep-alive',
-          'slot',
-        ],
-      },
-    ],
+          "component",
+          "template",
+          "transition",
+          "transition-group",
+          "keep-alive",
+          "slot"
+        ]
+      }
+    ]
   },
-  'plugins': [
-    'prettier',
-    'vue'
-  ]
-}
-
+  plugins: ["prettier", "vue"]
+};
