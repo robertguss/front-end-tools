9400079aadea5252e3b894d58747c76b7d9399a1
Robert Guss <robert.guss@insuremytrip.com>
2019-03-21 11:53:15 -0400

    configured jest to work with TS

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,13 @@
 module.exports = {
   collectCoverageFrom: ['src/**/*.{js,vue}', '!**/node_modules/**'],
   coverageDirectory: '<rootDir>/tests/unit/coverage',
-  moduleFileExtensions: ['js', 'json', 'vue'],
+  moduleFileExtensions: ['js', 'json', 'vue', 'ts'],
   roots: ['<rootDir>/src/', '<rootDir>/tests/'],
-  testMatch: ['**/(*.)spec.js'],
+  testMatch: ['**/(*.)spec.ts'],
   transform: {
     '^.+\\.vue$': 'vue-jest',
     '^.+\\.js$': 'babel-jest',
+    '^.+\\.ts?$': 'ts-jest',
   },
   verbose: true,
 };
